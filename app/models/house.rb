291ec2db2d89c096855bdaa9000b38621ed3fa59
Aymeric Azan <aymeric.azan@gmail.com>
2019-06-11 16:58:51 +0200

    create houses, coudinary

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,9 +1,9 @@
 class House < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   belongs_to :user
   has_many :bookings
 
-  validates :bedrooms, inclusion: { in: %w[1 2 3 4 5 6 7 8 9 10] }, presence: true
-  validates :capacity, inclusion: { in: %w[1 2 3 4 5 6 7 8 9 10] }, presence: true
+  validates :bedrooms, inclusion: { in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }, presence: true
+  validates :capacity, inclusion: { in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }, presence: true
   validates :category, inclusion: { in: %w[Entire_home Private_room Shared_room] }, presence: true
-
 end
