a10427ba1fa00ea9db1c53bad97b01f032bacc41
Gregorio Fracassi <gregorio.fracassi@gmail.com>
2019-06-11 15:04:25 +0200

    backend users sign-up and sign-in finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :photo])
   end
 end
