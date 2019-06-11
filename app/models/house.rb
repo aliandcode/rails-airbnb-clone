661b58146aa548b4859fca0f8ea7c40d51465169
Aymeric Azan <aymeric.azan@gmail.com>
2019-06-11 15:37:34 +0200

    Add dotenv - Protect my secret data in .env file

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,9 @@
 class House < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :bedrooms, inclusion: { in: %w[1 2 3 4 5 6 7 8 9 10] }, presence: true
+  validates :capacity, inclusion: { in: %w[1 2 3 4 5 6 7 8 9 10] }, presence: true
+  validates :category, inclusion: { in: %w[Entire_home Private_room Shared_room] }, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'houses#index'
+   resources :houses, only: [:index, :show, :new, :create]
 
   resources :houses
 end
