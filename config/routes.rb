31c510a7fb873ac8403c6b439566b8da48d252e6
adam somora <adamsomora33@gmail.com>
2019-06-11 18:40:11 +0200

    refactored the router

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'houses#index'
-   resources :houses, only: [:index, :show, :new, :create]
 
-  resources :houses do
+  resources :houses, only: [:index, :show, :new, :create] do
     resources :bookings, only: %i[show new create]
   end
 
